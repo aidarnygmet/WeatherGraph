99636aa85f3996f337a0586d3743da871b7bd90a
aidarnygmet <aidarn198@gmail.com>
2024-02-16 06:12:01 +0600

    fourth commit

--- src/app.js
+++ src/app.js
@@ -5,14 +5,21 @@ import './styles.css';
 import * as wijmo from '@grapecity/wijmo';
 import * as chart from '@grapecity/wijmo.chart';
 import * as interaction from '@grapecity/wijmo.chart.interaction';
-import { getData } from './data';
+import * as XLSX from 'node_modules/xlsx/dist/xlsx.full.min.js';
+import { fetchData, getData } from './data';
 //
 wijmo.setLicenseKey('aidar.nygmet@alumni.nu.edu.kz,E769971486566984#B0cJ5LlNHbhZmOiI7ckJye0ICbuFkI1pjIEJCLi4TP7dzYpFnNzVVQTZDOFxWTuJ4billZHRETQBnRhlnQ5FWQ8VjY4JDd7FUdmt6QLhFSzQlN7xGVnNXQrQnZqVVUTJWS6Z4aCVjatdUcRBFaNhzLzgDWvFVYXFmTx56c5cDMiRnQthERrU7TXZkRC5UeyZVbKV7T74EdMdGeYt4K4kXVzlmS0pXZwoXO84GNwFjYOlWaiJkZBNkYy3kSZ54Za36aDtETMlVRLFzQadFRO96ZFBlVFdUNRVWcGV6ZpdXT6hFcqxWeC3iV6EUcFBXerMDcURnNS9WSwMFU98WRTxWM7QzcBhFNzcjWXhlaL54SxZ6VxoVWQFDSyAzVyM6QYl6YyN5MqhVUxZXYvlHaB9EM7MXODl4NyImaMlnV8xkWHp4T4Y5ZyE6NmJ4U7AnNpd6SUVXcXlWY6JmbTRVMVNDexMkeml7MjFDRst4R4UEMKZEO9c6SiojITJCLiUDOyI4QxIDNiojIIJCLwEDOyAjN8UDO0IicfJye35XX3JSSwIjUiojIDJCLi86bpNnblRHeFBCI4VWZoNFelxmRg2Wbql6ViojIOJyes4nI5kkTRJiOiMkIsIibvl6cuVGd8VEIgIXZ7VWaWRncvBXZSBybtpWaXJiOi8kI1xSfis4N8gkI0IyQiwiIu3Waz9WZ4hXRgAydvJVa4xWdNBybtpWaXJiOi8kI1xSfiQjR6QkI0IyQiwiIu3Waz9WZ4hXRgACUBx4TgAybtpWaXJiOi8kI1xSfiMzQwIkI0IyQiwiIlJ7bDBybtpWaXJiOi8kI1xSfiUFO7EkI0IyQiwiIu3Waz9WZ4hXRgACdyFGaDxWYpNmbh9WaGBybtpWaXJiOi8kI1tlOiQmcQJCLiMDNyQDMxASNxIDM4IDMyIiOiQncDJCLiYTMzADNyAjMiojIwhXRiwiI0tmL5RWZuUnbukmbtVHbhBEdl56Z99mLyFGZpFmI0ISYONkIsUWdyRnOiwmdFJCLiQDO9YjN5YDO4EzN9kjN7IiOiQWSiwSfdtlOicGbmJCLiEjd4IDMyIiOxIIZ');
 document.readyState === 'complete' ? init() : window.onload = init;
 //
 function init() {
+    document.getElementById('fetchDataButton').addEventListener('click', async () => {
+        const data = await fetchData();
+        if (data) {
+            console.log(data)
+            generateAsExcel(data)
+        }
+    });
     getData().then(response =>{
-        console.log(response)
         let palette = ['rgba(55,90,127,1)', 'rgba(0,188,140,1)', 'rgba(52,152,219,1)', 'rgba(243,156,18,1)', 'rgba(231,76,60,1)', 'rgba(143,97,179,1)', 'rgba(176,135,37,1)', 'rgba(74,73,73,1)', 'rgba(0,0,0,1)']
         let theChart = new chart.FlexChart('#theChart', {
             itemsSource: response,
@@ -60,9 +67,30 @@ function init() {
             theChart.endUpdate();
         }
     });
-    })
- 
-   
-    
+    }) 
 }
+function generateAsExcel(data) {
+    try {
+      const workbook = XLSX.utils.book_new();
+      
+      for (let key in data) {
+        const worksheet = XLSX.utils.json_to_sheet(data[key]);
+        XLSX.utils.book_append_sheet(workbook, worksheet, key);
+      }
+      
+      let res = XLSX.write(workbook, { type: "array" });
+      const blob = new Blob([res], { type: "application/octet-stream" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        a.href = url;
+        a.download = "data.xlsx";
+        a.click();
+        window.URL.revokeObjectURL(url);
+      console.log(`${res.byteLength} bytes generated`);
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  }
 
--- src/data.js
+++ src/data.js
@@ -19,3 +19,44 @@ export async function getData() {
         
     // });
 }
+export async function fetchData() {
+    try {
+        const response = await fetch('http://localhost:8080/get/forecast_directly/Astana1');
+        const data = await response.json();
+        const res = data.map(entry=>{
+            return {
+                date: timestampToDate(entry.dt),
+                time: timestampToTime(entry.dt),
+                temp: entry.temp, 
+                feels_like: entry.feels_like, 
+                pressure: entry.pressure,
+                humidity: entry.humidity,
+                dew_point: entry.dew_point,
+                uvi: entry.uvi,
+                clouds: entry.clouds,
+                visibility: entry.visibility,
+                wind_speed: entry.wind_speed,
+                wind_deg: entry.wind_deg,
+                pop: entry.pop
+
+            }
+        })
+        return {key: res};
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
+function timestampToDate(timestamp) {
+    const date = new Date(timestamp*1000);
+    const day = date.getDate().toString().padStart(2, '0'); // Get day and pad with leading zero if necessary
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get month (zero-based) and pad with leading zero if necessary
+    const year = date.getFullYear().toString(); // Get full year
+    return `${day}/${month}/${year}`;
+}
+function timestampToTime(timestamp) {
+    const date = new Date(timestamp*1000);
+    const hours = date.getHours().toString().padStart(2, '0'); // Get hours and pad with leading zero if necessary
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes and pad with leading zero if necessary
+    return `${hours}:${minutes}`;
+}
